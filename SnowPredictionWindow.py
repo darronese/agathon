c704fd8b84e19243e5d0ee085c7e04e9740fef2e
Collin Bale <146786065+COB4YN@users.noreply.github.com>
2025-01-31 20:44:00 -0800

    Create SnowPredictionWindow.py

---
+++ SnowPredictionWindow.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+# Create the main window
+root = tk.Tk()
+root.title("My Window")  # Set window title
+root.geometry("400x300")  # Set window size (width x height)
+
+# Run the application
+root.mainloop()
