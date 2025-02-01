9bc1d02313d58f87ed0dcb7db33ead71077c081e
Neal Wang <nealwang.sh@protonmail.com>
2025-01-31 22:05:32 -0800

    feat: csv test

--- data/src/main.rs
+++ data/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let mut rdr =
+        csv::Reader::from_path("input_data/meteorological_data/Modified_Output_precip.csv")
+            .unwrap();
+
+    let line = rdr.records().next().unwrap().unwrap();
+
+    println!("{:?}", line);
 }
