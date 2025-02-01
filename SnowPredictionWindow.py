d3e185a783f7f21228061fd9b77bda19c62bfad8
Collin Bale <146786065+COB4YN@users.noreply.github.com>
2025-01-31 21:02:09 -0800

    Update SnowPredictionWindow.py
    Grid setup: columns and rows

--- SnowPredictionWindow.py
+++ SnowPredictionWindow.py
@@ -1,9 +1,16 @@
-import tkinter as tk
+from tkinter import *
+from tkinter import ttk
 
 # Create the main window
-root = tk.Tk()
-root.title("My Window")  # Set window title
+root = Tk()
+root.title("Snowpack Prediction")  # Set window title
 root.geometry("400x300")  # Set window size (width x height)
+
+# Create frame
+mainframe = ttk.Frame(root, padding = "3 3 12 12")
+mainframe.gride(column=0, row=0, sticky = (N, W, E, S))
+root.columnconfigure(0, weight = 1)
+root.rowconfigure(0, weight = 1)
 
 # Run the application
 root.mainloop()
